3ba6b0cfdf3a20a815420add283893705582a96e
Katharine Berry <katharine@getpebble.com>
2016-01-20 02:29:04 -0800

    ugh unicode.

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ __author__ = 'katharine'
 from os import environ as _environ
 
 RESPONSE_TIME_LIMIT = int(_environ.get('RESPONSE_TIME_LIMIT', 8))
-TELEGRAM_TOKEN = _environ.get('TELEGRAM_TOKEN')
-IMGUR_TOKEN = _environ.get('IMGUR_TOKEN')
-REDIS_URL = _environ.get('REDIS_URL')
+TELEGRAM_TOKEN = _environ.get('TELEGRAM_TOKEN').decode('utf-8')
+IMGUR_TOKEN = _environ.get('IMGUR_TOKEN').decode('utf-8')
+REDIS_URL = _environ.get('REDIS_URL').decode('utf-8')
 CACHE_TIME = int(_environ.get('CACHE_TIME', 86400))
 PARTIAL_RESULT_CACHE_TIME = int(_environ.get('PARTIAL_RESULT_CACHE_TIME', 20))
 PORT = int(_environ.get('PORT', 5000))
